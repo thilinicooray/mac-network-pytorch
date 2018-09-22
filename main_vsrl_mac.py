cf7cd7e79c94be22aa23ad8bd3b750948844e75e
thilinicooray <thilinicooray.ucsc@gmail.com>
2018-09-22 16:48:03 +0800

    layer 4 mac pred v for training top 5 choice

--- main_vsrl_mac.py
+++ main_vsrl_mac.py
@@ -74,14 +74,15 @@ def train(model, train_loader, dev_loader, traindev_loader, optimizer, scheduler
             print('=========================================================================')
             print(labels)'''
 
-            verb_predict, role_predict = pmodel(img, verb, roles)
+            #verb_predict, role_predict = pmodel(img, verb, roles)
+            verb_predict, role_predict = pmodel.forward_eval5(img)
             #print ("forward time = {}".format(time.time() - t1))
             t1 = time.time()
 
             '''g = make_dot(verb_predict, model.state_dict())
             g.view()'''
 
-            loss = model.calculate_loss(verb_predict, verb, role_predict, labels, args)
+            loss = model.calculate_eval_loss(verb_predict, verb, role_predict, labels, args)
             #loss = loss_ * random.random() #try random loss
             #print ("loss time = {}".format(time.time() - t1))
             t1 = time.time()
@@ -112,8 +113,8 @@ def train(model, train_loader, dev_loader, traindev_loader, optimizer, scheduler
 
             train_loss += loss.item()
 
-            top1.add_point(verb_predict, verb, role_predict, labels)
-            top5.add_point(verb_predict, verb, role_predict, labels)
+            top1.add_point_eval5(verb_predict, verb, role_predict, labels)
+            top5.add_point_eval5(verb_predict, verb, role_predict, labels)
 
 
             if total_steps % print_freq == 0:
@@ -145,7 +146,7 @@ def train(model, train_loader, dev_loader, traindev_loader, optimizer, scheduler
                 max_score = max(dev_score_list)
 
                 if max_score == dev_score_list[-1]:
-                    torch.save(model.state_dict(), model_dir + "/{}_macnet4layer_selfatt.model".format( model_name))
+                    torch.save(model.state_dict(), model_dir + "/{}_macnet4layer_predvb.model".format( model_name))
                     print ('New best model saved! {0}'.format(max_score))
 
                 #eval on the trainset
