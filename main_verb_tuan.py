7c89a620b8dc7daa8297989c8fc52423ec20502a
thilinicooray <thilinicooray.ucsc@gmail.com>
2018-09-28 23:19:28 +0800

    verb ranking

--- main_verb_tuan.py
+++ main_verb_tuan.py
@@ -82,8 +82,9 @@ def train(model, train_loader, dev_loader, traindev_loader, optimizer, scheduler
 
             p_loss = model.calculate_loss(predicted_labels, gt_labels)
             #n_loss = model.calculate_loss(n_img_pred, n_verb)
-            triplet_loss = model.triplet_loss(p_img_rep, p_verb_rep, n_img_rep, n_verb_rep)
-            loss = triplet_loss + p_loss
+            #triplet_loss = model.triplet_loss(p_img_rep, p_verb_rep, n_img_rep, n_verb_rep)
+            #loss = triplet_loss + p_loss
+            loss = p_loss
             #loss = triplet_loss
             #print('current loss = ', loss)
 
