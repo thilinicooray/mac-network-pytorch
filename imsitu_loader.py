0fa0a13de5545b4c244210543b3d60ecf0ec0eb9
thilinicooray <thilinicooray.ucsc@gmail.com>
2018-10-15 17:03:17 +0800

    trying lstm

--- imsitu_loader.py
+++ imsitu_loader.py
@@ -23,3 +23,23 @@ class imsitu_loader(data.Dataset):
 
     def __len__(self):
         return len(self.annotations)
+
+class imsitu_loader_roleq(data.Dataset):
+    def __init__(self, img_dir, annotation_file, encoder, transform=None):
+        self.img_dir = img_dir
+        self.annotations = annotation_file
+        self.ids = list(self.annotations.keys())
+        self.encoder = encoder
+        self.transform = transform
+
+    def __getitem__(self, index):
+        _id = self.ids[index]
+        ann = self.annotations[_id]
+        img = Image.open(os.path.join(self.img_dir, _id)).convert('RGB')
+        #transform must be None in order to give it as a tensor
+        if self.transform is not None: img = self.transform(img)
+        verb, roles, roleq, q_len, labels = self.encoder.encode(ann)
+        return _id, img, verb, roles, roleq, q_len, labels
+
+    def __len__(self):
+        return len(self.annotations)
