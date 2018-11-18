c1586132054ee9865157ce31928a6c648651ff57
thilinicooray <thilinicooray.ucsc@gmail.com>
2018-11-18 12:00:28 +0800

    fuse with frcnn feat

--- main_roleread_try_with_frcnn.py
+++ main_roleread_try_with_frcnn.py
@@ -152,7 +152,7 @@ def train(model, train_loader, dev_loader, traindev_loader, optimizer, scheduler
                 max_score = max(dev_score_list)
 
                 if max_score == dev_score_list[-1]:
-                    torch.save(model.state_dict(), model_dir + "/{}_mac_roleread_testing_with_linearinit.model".format( model_name))
+                    torch.save(model.state_dict(), model_dir + "/{}_mac_roleread_testing_with_frcnnfuse.model".format( model_name))
                     print ('New best model saved! {0}'.format(max_score))
 
                 #eval on the trainset
