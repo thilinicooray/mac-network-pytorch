28a7a6e058a92730c6dea6bbe861e44b4455326f
thilinicooray <thilinicooray.ucsc@gmail.com>
2018-11-27 16:37:34 +0800

    frcnn with q

--- imsitu_loader.py
+++ imsitu_loader.py
@@ -98,6 +98,38 @@ class imsitu_loader_frcnn(data.Dataset):
     def __len__(self):
         return len(self.annotations)
 
+class imsitu_loader_frcnn_roleq(data.Dataset):
+    def __init__(self, img_dir, frcnn_feat_dir, annotation_file, encoder, transform=None):
+        self.img_dir = img_dir
+        self.frcnn_feat_dir = frcnn_feat_dir
+        self.annotations = annotation_file
+        self.ids = list(self.annotations.keys())
+        self.encoder = encoder
+        self.transform = transform
+
+    def __getitem__(self, index):
+        #print('current idx :' , index)
+        _id = self.ids[index]
+        if index % 2 == 0:
+            _id = 'pitching_187.jpg'
+        else:
+            _id = 'shouting_260.jpg'
+        ann = self.annotations[_id]
+        img = Image.open(os.path.join(self.img_dir, _id)).convert('RGB')
+        #transform must be None in order to give it as a tensor
+        if self.transform is not None: img = self.transform(img)
+        frcnn_feat = torch.from_numpy(np.load(os.path.join(self.frcnn_feat_dir,
+                                                           _id.replace('jpg', 'npy'))))
+        verb, roles, roleq, q_len, labels = self.encoder.encode(ann)
+
+        #print('feat loader:', frcnn_feat.size())
+
+        return _id, img, frcnn_feat, verb, roles, roleq, q_len, labels
+
+    def __len__(self):
+        return len(self.annotations)
+
+
 def shuffle_minibatch(batch):
     random.shuffle(batch)
     ids, imgs, verbs, role_set, label_set = [],[],[],[],[]
