750a1f21e8d83b9e842c4249648f78886a1dbc63
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-01-02 05:24:08 +0530

    with context once

--- imsitu_loader.py
+++ imsitu_loader.py
@@ -67,6 +67,26 @@ class imsitu_loader_verbq(data.Dataset):
     def __len__(self):
         return len(self.annotations)
 
+class imsitu_loader_verb_roleq(data.Dataset):
+    def __init__(self, img_dir, annotation_file, encoder, transform=None):
+        self.img_dir = img_dir
+        self.annotations = annotation_file
+        self.ids = list(self.annotations.keys())
+        self.encoder = encoder
+        self.transform = transform
+
+    def __getitem__(self, index):
+        _id = self.ids[index]
+        ann = self.annotations[_id]
+        img = Image.open(os.path.join(self.img_dir, _id)).convert('RGB')
+        #transform must be None in order to give it as a tensor
+        if self.transform is not None: img = self.transform(img)
+        verb, verbq, labels = self.encoder.encode(ann)
+        return _id, img, verb, verbq, labels
+
+    def __len__(self):
+        return len(self.annotations)
+
 class imsitu_loader_frcnn(data.Dataset):
     def __init__(self, img_dir, frcnn_feat_dir, annotation_file, encoder, transform=None):
         self.img_dir = img_dir
