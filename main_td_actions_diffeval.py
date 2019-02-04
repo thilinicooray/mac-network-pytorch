3c367eb6ed92b7600c957033fca95c71bdd14373
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-02-04 15:28:36 +0800

    td action diff eval

--- main_td_actions_diffeval.py
+++ main_td_actions_diffeval.py
@@ -12,7 +12,7 @@ import random
 #from graphviz import Digraph
 
 
-def train(model, train_loader, dev_loader, traindev_loader, optimizer, scheduler, max_epoch, model_dir, encoder, gpu_mode, clip_norm, lr_max, model_name, args,eval_frequency=1000):
+def train(model, train_loader, dev_loader, traindev_loader, optimizer, scheduler, max_epoch, model_dir, encoder, gpu_mode, clip_norm, lr_max, model_name, args,eval_frequency=4000):
     model.train()
     train_loss = 0
     total_steps = 0
