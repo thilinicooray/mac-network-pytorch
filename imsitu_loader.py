2c3de3cd53d79c7a495179360b3f36af026dfe83
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-02-13 01:09:21 +0800

    365 places for verb

--- imsitu_loader.py
+++ imsitu_loader.py
@@ -132,6 +132,26 @@ class imsitu_loader_verbq_mul(data.Dataset):
     def __len__(self):
         return len(self.annotations)
 
+class imsitu_loader_place4verbq(data.Dataset):
+    def __init__(self, img_dir, annotation_file, encoder, transform=None):
+        self.img_dir = img_dir
+        self.annotations = annotation_file
+        self.ids = list(self.annotations.keys())
+        self.encoder = encoder
+        self.transform = transform
+
+    def __getitem__(self, index):
+        _id = self.ids[index]
+        ann = self.annotations[_id]
+        img = Image.open(os.path.join(self.img_dir, _id)).convert('RGB')
+        #transform must be None in order to give it as a tensor
+        if self.transform is not None: img = self.transform(img)
+        verb, verbq = self.encoder.encode(ann, _id)
+        return _id, img, verb, verbq
+
+    def __len__(self):
+        return len(self.annotations)
+
 class imsitu_loader_rotation(data.Dataset):
     def __init__(self, img_dir, annotation_file, encoder, transform=None):
         self.img_dir = img_dir
