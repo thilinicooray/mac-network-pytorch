24f37561b3edbd56a58890c55531ceb20798cb6b
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-02-17 13:22:20 +0800

    agent simple

---
+++ model_agent_simple.py
@@ -0,0 +1,101 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+import utils
+import torchvision as tv
+
+class vgg16_modified(nn.Module):
+    def __init__(self, num_classes):
+        super(vgg16_modified, self).__init__()
+        vgg = tv.models.vgg16_bn(pretrained=True)
+        self.vgg_features = vgg.features
+
+        for param in self.vgg_features.parameters():
+            param.require_grad = False
+
+        self.out_features = vgg.classifier[6].in_features
+        features = list(vgg.classifier.children())[:-1] # Remove last layer
+        features.extend([nn.Linear(self.out_features, num_classes)])
+        self.vgg_classifier = nn.Sequential(*features) # Replace the model classifier
+        #print(self.vgg_classifier)
+
+    def rep_size(self):
+        return 1024
+
+    def base_size(self):
+        return 512
+
+    def forward(self,x):
+        y =  self.vgg_classifier(self.vgg_features(x).view(-1, 512*7*7))
+        #print('y size :',  y.size())
+        return y
+
+class BaseModel(nn.Module):
+    def __init__(
+            self,
+            encoder,
+            gpu_mode,
+            conv_hidden=24,
+            embed_hidden=300,
+            lstm_hidden=300,
+            mlp_hidden=512
+    ):
+        super(BaseModel, self).__init__()
+
+        self.normalize = tv.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+
+        self.train_transform = tv.transforms.Compose([
+            tv.transforms.RandomRotation(15),
+            tv.transforms.RandomResizedCrop(224),
+            tv.transforms.RandomHorizontalFlip(),
+            tv.transforms.ToTensor(),
+            self.normalize,
+        ])
+
+        self.dev_transform = tv.transforms.Compose([
+            tv.transforms.Resize(224),
+            tv.transforms.CenterCrop(224),
+            tv.transforms.ToTensor(),
+            self.normalize,
+        ])
+
+        self.encoder = encoder
+        self.gpu_mode = gpu_mode
+        self.vocab_size = self.encoder.get_num_labels()
+
+        self.conv = vgg16_modified(self.vocab_size)
+
+
+
+    def train_preprocess(self):
+        return self.train_transform
+
+    def dev_preprocess(self):
+        return self.dev_transform
+
+    def forward(self, image):
+
+        '''print('testing 123')
+        x = torch.tensor([[1, 2, 3],[4,5,6]])
+        print('original', x.size())
+        x = x.repeat(1,2)
+        print('xxxxxx', x, x.view(-1,3), x.size())'''
+
+        verb_pred = self.conv(image)
+
+        #verb pred
+
+        return verb_pred
+
+
+    def calculate_loss(self, agent_pred, gt_labels):
+
+        batch_size = agent_pred.size()[0]
+        loss = 0
+        for i in range(batch_size):
+            for index in range(gt_labels.size()[1]):
+                loss += utils.cross_entropy_loss(agent_pred[i], gt_labels[i,index])
+
+        final_loss = loss/batch_size
+        #print('loss :', final_loss)
+        return final_loss
