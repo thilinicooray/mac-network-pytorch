4987ed3c237219ad3b0afebb9d0d4b1387534504
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-02-23 14:34:58 +0800

    verb model 40noise

--- main_verbq_flatmlp_nrm_reuseclss.py
+++ main_verbq_flatmlp_nrm_reuseclss.py
@@ -90,7 +90,7 @@ def train(model, train_loader, dev_loader, traindev_loader, optimizer, scheduler
             loss.backward()
             #print ("backward time = {}".format(time.time() - t1))
 
-            #torch.nn.utils.clip_grad_norm_(model.parameters(), clip_norm)
+            torch.nn.utils.clip_grad_norm_(model.parameters(), clip_norm)
 
 
             '''for param in filter(lambda p: p.requires_grad,model.parameters()):
@@ -148,7 +148,7 @@ def train(model, train_loader, dev_loader, traindev_loader, optimizer, scheduler
                 max_score = max(dev_score_list)
 
                 if max_score == dev_score_list[-1]:
-                    torch.save(model.state_dict(), model_dir + "/{}_verbq_flatmlp_reuseclss_nrm.model".format( model_name))
+                    torch.save(model.state_dict(), model_dir + "/{}_verbq_flatmlp_reuseclss_nrm_clip.model".format( model_name))
                     print ('New best model saved! {0}'.format(max_score))
 
                 #eval on the trainset
@@ -248,7 +248,7 @@ def main():
     lr_max = 5e-4
     lr_gamma = 0.1
     lr_step = 25
-    clip_norm = 50
+    clip_norm = 0.5
     weight_decay = 1e-4
     n_epoch = 500
     n_worker = 3
