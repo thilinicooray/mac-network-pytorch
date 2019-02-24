386b10ea7813f2e1881cd970e99174ccc1dd06e9
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-02-24 18:57:38 +0800

    roles 40

--- classifier.py
+++ classifier.py
@@ -16,3 +16,19 @@ class SimpleClassifier(nn.Module):
     def forward(self, x):
         logits = self.main(x)
         return logits
+
+class SimpleClassifierBN(nn.Module):
+    def __init__(self, in_dim, hid_dim, out_dim, dropout):
+        super(SimpleClassifierBN, self).__init__()
+        layers = [
+            weight_norm(nn.Linear(in_dim, hid_dim), dim=None),
+            nn.BatchNorm1d(hid_dim),
+            nn.ReLU(),
+            nn.Dropout(dropout, inplace=True),
+            weight_norm(nn.Linear(hid_dim, out_dim), dim=None)
+        ]
+        self.main = nn.Sequential(*layers)
+
+    def forward(self, x):
+        logits = self.main(x)
+        return logits
