dbb8d3679f6fcde0eecab5707c26463bfe50c4de
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-02-25 23:32:39 +0800

    role 40

--- model_roles_independent_objdet.py
+++ model_roles_independent_objdet.py
@@ -12,7 +12,7 @@ import numpy as np
 class vgg16_modified(nn.Module):
     def __init__(self):
         super(vgg16_modified, self).__init__()
-        vgg = tv.models.vgg16_bn(pretrained=True)
+        vgg = tv.models.vgg16(pretrained=True)
         self.vgg_features = vgg.features
 
     def rep_size(self):
@@ -40,7 +40,7 @@ class TopDown(nn.Module):
         self.q_emb = nn.LSTM(embed_hidden, mlp_hidden,
                              batch_first=True, bidirectional=True)
         self.lstm_proj = nn.Linear(mlp_hidden * 2, mlp_hidden)
-        self.v_att = Attention(mlp_hidden, mlp_hidden, mlp_hidden)
+        self.v_att = NewAttention(mlp_hidden, mlp_hidden, mlp_hidden)
         self.q_net = FCNet([mlp_hidden, mlp_hidden])
         self.v_net = FCNet([mlp_hidden, mlp_hidden])
         self.classifier = SimpleClassifier(
