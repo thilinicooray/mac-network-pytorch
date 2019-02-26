75e5987ef6a46312f70b4c77d671340b1e12d239
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-02-27 01:02:25 +0800

    role 40

---
+++ model_roles_depggnn.py
@@ -0,0 +1,306 @@
+import torch
+import torch.nn as nn
+from attention import Attention, NewAttention
+from language_model import WordEmbedding, QuestionEmbedding
+from classifier import SimpleClassifier
+from fc import FCNet
+import torch.nn.functional as F
+import torchvision as tv
+import utils
+import numpy as np
+from torch.nn.parameter import Parameter
+from torch.nn.init import kaiming_uniform_, xavier_uniform_, normal
+import math
+
+def linear(in_dim, out_dim, bias=True):
+    lin = nn.Linear(in_dim, out_dim, bias=bias)
+    xavier_uniform_(lin.weight)
+    if bias:
+        lin.bias.data.zero_()
+
+    return lin
+
+class vgg16_modified(nn.Module):
+    def __init__(self):
+        super(vgg16_modified, self).__init__()
+        vgg = tv.models.vgg16(pretrained=True)
+        self.vgg_features = vgg.features
+
+    def rep_size(self):
+        return 1024
+
+    def base_size(self):
+        return 512
+
+    def forward(self,x):
+        #return self.dropout2(self.relu2(self.lin2(self.dropout1(self.relu1(self.lin1(self.vgg_features(x).view(-1, 512*7*7)))))))
+        features = self.vgg_features(x)
+
+        return features
+
+class AttrProxy(object):
+    """
+    Translates index lookups into attribute lookups.
+    To implement some trick which able to use list of nn.Module in a nn.Module
+    see https://discuss.pytorch.org/t/list-of-nn-module-in-a-nn-module/219/2
+    """
+    def __init__(self, module, prefix):
+        self.module = module
+        self.prefix = prefix
+
+    def __getitem__(self, i):
+        return getattr(self.module, self.prefix + str(i))
+
+
+class Propogator(nn.Module):
+    """
+    Gated Propogator for GGNN
+    Using LSTM gating mechanism
+    """
+    def __init__(self, state_dim, n_node, n_edge_types):
+        super(Propogator, self).__init__()
+
+        self.n_node = n_node
+        self.n_edge_types = n_edge_types
+
+        self.reset_gate = nn.Sequential(
+            linear(state_dim*3, state_dim),
+            nn.Sigmoid()
+        )
+        self.update_gate = nn.Sequential(
+            linear(state_dim*3, state_dim),
+            nn.Sigmoid()
+        )
+        self.tansform = nn.Sequential(
+            linear(state_dim*3, state_dim),
+            nn.Tanh()
+        )
+
+    def forward(self, state_in, state_out, state_cur, A):
+        A_in = A[:, :, :self.n_node*self.n_edge_types]
+        A_out = A[:, :, :self.n_node*self.n_edge_types]
+
+        a_in = torch.bmm(A_in, state_in)
+        a_out = torch.bmm(A_out, state_out)
+        a = torch.cat((a_in, a_out, state_cur), 2)
+
+        r = self.reset_gate(a)
+        z = self.update_gate(a)
+        joined_input = torch.cat((a_in, a_out, r * state_cur), 2)
+        h_hat = self.tansform(joined_input)
+
+        output = (1 - z) * state_cur + z * h_hat
+
+        return output
+
+
+class GGNN(nn.Module):
+    """
+    Gated Graph Sequence Neural Networks (GGNN)
+    Mode: SelectNode
+    Implementation based on https://arxiv.org/abs/1511.05493
+    """
+    def __init__(self, state_dim, n_edge_types,n_node,  n_steps):
+        super(GGNN, self).__init__()
+
+        self.state_dim = state_dim
+        self.n_edge_types = n_edge_types
+        self.n_node = n_node
+        self.n_steps = n_steps
+
+        for i in range(self.n_edge_types):
+            # incoming and outgoing edge embedding
+            in_fc = linear(self.state_dim, self.state_dim)
+            out_fc = linear(self.state_dim, self.state_dim)
+            self.add_module("in_{}".format(i), in_fc)
+            self.add_module("out_{}".format(i), out_fc)
+
+        self.in_fcs = AttrProxy(self, "in_")
+        self.out_fcs = AttrProxy(self, "out_")
+
+        # Propogation Model
+        self.propogator = Propogator(self.state_dim, self.n_node, self.n_edge_types)
+
+    def forward(self, prop_state, A):
+        for i_step in range(self.n_steps):
+            in_states = []
+            out_states = []
+            for i in range(self.n_edge_types):
+                in_states.append(self.in_fcs[i](prop_state))
+                out_states.append(self.out_fcs[i](prop_state))
+            in_states = torch.stack(in_states).transpose(0, 1).contiguous()
+            in_states = in_states.view(-1, self.n_node*self.n_edge_types, self.state_dim)
+            out_states = torch.stack(out_states).transpose(0, 1).contiguous()
+            out_states = out_states.view(-1, self.n_node*self.n_edge_types, self.state_dim)
+
+            prop_state = self.propogator(in_states, out_states, prop_state, A)
+
+        return prop_state
+
+class TopDown(nn.Module):
+    def __init__(self,
+                 vocab_size,
+                 embed_hidden=300,
+                 mlp_hidden=512):
+        super(TopDown, self).__init__()
+
+        self.vocab_size = vocab_size
+
+        self.q_emb = nn.LSTM(embed_hidden, mlp_hidden,
+                             batch_first=True, bidirectional=True)
+        self.lstm_proj = nn.Linear(mlp_hidden * 2, mlp_hidden)
+        self.verb_transform = nn.Linear(embed_hidden, mlp_hidden)
+        self.v_att = Attention(mlp_hidden, mlp_hidden, mlp_hidden)
+        self.q_net = FCNet([mlp_hidden, mlp_hidden])
+        self.v_net = FCNet([mlp_hidden, mlp_hidden])
+
+    def forward(self, img, q):
+        batch_size = img.size(0)
+        w_emb = q
+        self.q_emb.flatten_parameters()
+        lstm_out, (h, _) = self.q_emb(w_emb)
+        q_emb = h.permute(1, 0, 2).contiguous().view(batch_size, -1)
+        q_emb = self.lstm_proj(q_emb)
+
+        att = self.v_att(img, q_emb)
+        v_emb = (att * img).sum(1) # [batch, v_dim]
+
+        q_repr = self.q_net(q_emb)
+        v_repr = self.v_net(v_emb)
+        joint_repr = q_repr * v_repr
+
+        return joint_repr
+
+class BaseModel(nn.Module):
+    def __init__(self, encoder,
+                 gpu_mode,
+                 embed_hidden=300,
+                 mlp_hidden=512):
+        super(BaseModel, self).__init__()
+
+        self.normalize = tv.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+
+        self.train_transform = tv.transforms.Compose([
+            tv.transforms.RandomRotation(10),
+            tv.transforms.RandomResizedCrop(224),
+            tv.transforms.RandomHorizontalFlip(),
+            tv.transforms.ToTensor(),
+            self.normalize,
+        ])
+
+        self.dev_transform = tv.transforms.Compose([
+            tv.transforms.Resize(224),
+            tv.transforms.CenterCrop(224),
+            tv.transforms.ToTensor(),
+            self.normalize,
+        ])
+
+        self.encoder = encoder
+        self.gpu_mode = gpu_mode
+        self.n_roles = self.encoder.get_num_roles()
+        self.n_verbs = self.encoder.get_num_verbs()
+        self.vocab_size = self.encoder.get_num_labels()
+        self.max_role_count = self.encoder.get_max_role_count()
+        self.n_role_q_vocab = len(self.encoder.question_words)
+
+        self.conv = vgg16_modified()
+
+        self.conv_hidden = self.conv.base_size()
+        self.mlp_hidden = mlp_hidden
+        self.embed_hidden = embed_hidden
+        self.verb_lookup = nn.Embedding(self.n_verbs, embed_hidden)
+        self.w_emb = nn.Embedding(self.n_role_q_vocab + 1, embed_hidden, padding_idx=self.n_role_q_vocab)
+        self.roles = TopDown(self.vocab_size)
+
+        self.label_proj = nn.Linear(self.embed_hidden+self.mlp_hidden, self.mlp_hidden)
+
+        self.ggnn = GGNN(state_dim=self.mlp_hidden, n_edge_types=1,n_node=self.max_role_count,
+                        n_steps=2)
+
+        self.classifier = SimpleClassifier(
+            mlp_hidden, 2 * mlp_hidden, self.vocab_size, 0.5)
+
+
+
+    def train_preprocess(self):
+        return self.train_transform
+
+    def dev_preprocess(self):
+        return self.dev_transform
+
+    def forward(self, img, verb):
+
+        img_features = self.conv(img)
+        batch_size, n_channel, conv_h, conv_w = img_features.size()
+        img = img_features.view(batch_size, n_channel, -1)
+        img = img.permute(0, 2, 1)
+
+        img = img.expand(self.max_role_count,img.size(0), img.size(1), img.size(2))
+        img = img.transpose(0,1)
+        img = img.contiguous().view(batch_size* self.max_role_count, -1, self.mlp_hidden)
+
+        role_qs, _ = self.encoder.get_role_questions_batch(verb)
+        if self.gpu_mode >= 0:
+            role_qs = role_qs.to(torch.device('cuda'))
+
+        role_qs = role_qs.view(batch_size*self.max_role_count, -1)
+        embed_qs = self.w_emb(role_qs)
+
+        rep = self.roles(img, embed_qs)
+
+        role_label_rep = rep.contiguous().view(batch_size, -1, self.mlp_hidden)
+
+        verb_embd = self.verb_lookup(verb)
+        verb_embed_expand = verb_embd.expand(self.max_role_count, verb_embd.size(0), verb_embd.size(1))
+        verb_embed_expand = verb_embed_expand.transpose(0,1)
+
+        role_rep_verb = torch.cat([role_label_rep, verb_embed_expand],-1)
+
+        adj = torch.ones([batch_size, self.max_role_count, self.max_role_count], dtype=torch.float)
+        if self.gpu_mode >= 0:
+            adj = adj.to(torch.device('cuda'))
+
+        role_rep_verb = self.label_proj(role_rep_verb)
+
+        updated_nodes = self.ggnn(role_rep_verb, adj)
+
+        role_tot = updated_nodes
+        role_label_pred = self.classifier(role_tot)
+
+
+
+        return role_label_pred
+
+    def calculate_loss(self, gt_verbs, role_label_pred, gt_labels,args):
+
+        batch_size = role_label_pred.size()[0]
+        if args.train_all:
+            loss = 0
+            for i in range(batch_size):
+                for index in range(gt_labels.size()[1]):
+                    frame_loss = 0
+                    #verb_loss = utils.cross_entropy_loss(verb_pred[i], gt_verbs[i])
+                    #frame_loss = criterion(role_label_pred[i], gt_labels[i,index])
+                    for j in range(0, self.max_role_count):
+                        frame_loss += utils.cross_entropy_loss(role_label_pred[i][j], gt_labels[i,index,j] ,self.vocab_size)
+                    frame_loss = frame_loss/len(self.encoder.verb2_role_dict[self.encoder.verb_list[gt_verbs[i]]])
+                    #print('frame loss', frame_loss, 'verb loss', verb_loss)
+                    loss += frame_loss
+        else:
+            #verb from pre-trained
+            loss = 0
+            for i in range(batch_size):
+                for index in range(gt_labels.size()[1]):
+                    frame_loss = 0
+                    #verb_loss = utils.cross_entropy_loss(verb_pred[i], gt_verbs[i])
+                    #frame_loss = criterion(role_label_pred[i], gt_labels[i,index])
+                    for j in range(0, self.max_role_count):
+                        frame_loss += utils.cross_entropy_loss(role_label_pred[i][j], gt_labels[i,index,j] ,self.vocab_size)
+                    frame_loss = frame_loss/len(self.encoder.verb2_role_dict[self.encoder.verb_list[gt_verbs[i]]])
+                    #print('frame loss', frame_loss, 'verb loss', verb_loss)
+                    loss += frame_loss
+
+
+        final_loss = loss/batch_size
+        #print('loss :', final_loss)
+        return final_loss
