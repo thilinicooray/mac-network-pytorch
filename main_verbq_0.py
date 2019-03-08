eb257b0f449d0a253807c8def74d2a4f056431e5
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-03-08 22:28:44 +0800

    vqa verb iter 0

--- main_verbq_0.py
+++ main_verbq_0.py
@@ -3,7 +3,7 @@ from imsitu_encoder_verbq import imsitu_encoder
 from imsitu_loader import imsitu_loader_roleq_updated
 from imsitu_scorer_log import imsitu_scorer
 import json
-import model_verbq_0
+import model_verbq0_notextencoder
 import os
 import utils
 import time
@@ -148,7 +148,7 @@ def train(model, train_loader, dev_loader, traindev_loader, optimizer, scheduler
                 max_score = max(dev_score_list)
 
                 if max_score == dev_score_list[-1]:
-                    torch.save(model.state_dict(), model_dir + "/{}_verbq_firstiter.model".format( model_name))
+                    torch.save(model.state_dict(), model_dir + "/{}_verbq_firstiter_nolstm.model".format( model_name))
                     print ('New best model saved! {0}'.format(max_score))
 
                 #eval on the trainset
@@ -264,7 +264,7 @@ def main():
     imsitu_roleq = json.load(open("imsitu_data/imsitu_questions_prev.json"))
     encoder = imsitu_encoder(train_set, imsitu_roleq)
 
-    model = model_verbq_0.BaseModel(encoder, args.gpuid)
+    model = model_verbq0_notextencoder.BaseModel(encoder, args.gpuid)
 
     # To group up the features
     #cnn_features, role_features = utils.group_features_noun(model)
