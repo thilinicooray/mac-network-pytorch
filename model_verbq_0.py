329ce5e53e2ad33941fe61fae0884d1ea54906ba
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-03-12 13:44:36 +0800

    no roles

--- model_verbq_0.py
+++ model_verbq_0.py
@@ -42,7 +42,6 @@ class TopDown(nn.Module):
         self.q_emb = nn.LSTM(embed_hidden, mlp_hidden,
                              batch_first=True, bidirectional=True)
         self.lstm_proj = nn.Linear(mlp_hidden * 2, mlp_hidden)
-        self.verb_transform = nn.Linear(embed_hidden, mlp_hidden)
         self.v_att = NewAttention(mlp_hidden, mlp_hidden, mlp_hidden)
         '''self.q_net = FCNet([mlp_hidden, mlp_hidden])
         self.v_net = FCNet([mlp_hidden, mlp_hidden])
