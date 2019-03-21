696bcc948aa08d2631cbd18c94324e37216758d8
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-03-22 01:47:10 +0800

    2 lstm

--- model_verbq_iter_rolelike.py
+++ model_verbq_iter_rolelike.py
@@ -151,44 +151,41 @@ class BaseModel(nn.Module):
         verb_pred_prev = self.last_class(verb_pred_rep_prev)
 
         loss1 = self.calculate_loss(verb_pred_prev, verbs)
-        sum_losses = loss1
 
-        for i in range(3):
+        verb_q_idx_1 = self.encoder.get_verbq_idx(verbs, labels[:,0,:]).unsqueeze(1)
+        verb_q_idx_2 = self.encoder.get_verbq_idx(verbs, labels[:,1,:]).unsqueeze(1)
+        verb_q_idx_3 = self.encoder.get_verbq_idx(verbs, labels[:,2,:]).unsqueeze(1)
+        verb_q_idx = torch.cat([verb_q_idx_1, verb_q_idx_2, verb_q_idx_3], 1)
 
-            verb_q_idx_1 = self.encoder.get_verbq_idx(verbs, labels[:,0,:]).unsqueeze(1)
-            verb_q_idx_2 = self.encoder.get_verbq_idx(verbs, labels[:,1,:]).unsqueeze(1)
-            verb_q_idx_3 = self.encoder.get_verbq_idx(verbs, labels[:,2,:]).unsqueeze(1)
-            verb_q_idx = torch.cat([verb_q_idx_1, verb_q_idx_2, verb_q_idx_3], 1)
+        if self.gpu_mode >= 0:
+            verb_q_idx = verb_q_idx.to(torch.device('cuda'))
 
-            if self.gpu_mode >= 0:
-                verb_q_idx = verb_q_idx.to(torch.device('cuda'))
+        '''img_embd = self.conv(img)
+        batch_size, n_channel, conv_h, conv_w = img_embd.size()
+        img_embd = img_embd.view(batch_size, n_channel, -1)
+        img_embd = img_embd.permute(0, 2, 1)'''
+        img_embd = img_embd.expand(3,img_embd.size(0), img_embd.size(1), img_embd.size(2))
+        img_embd = img_embd.transpose(0,1)
+        img_embd = img_embd.contiguous().view(batch_size* 3, -1, self.mlp_hidden)
 
-            '''img_embd = self.conv(img)
-            batch_size, n_channel, conv_h, conv_w = img_embd.size()
-            img_embd = img_embd.view(batch_size, n_channel, -1)
-            img_embd = img_embd.permute(0, 2, 1)'''
-            img_embd = img_embd.expand(3,img_embd.size(0), img_embd.size(1), img_embd.size(2))
-            img_embd = img_embd.transpose(0,1)
-            img_embd = img_embd.contiguous().view(batch_size* 3, -1, self.mlp_hidden)
+        verb_q_idx = verb_q_idx.view(batch_size*3, -1)
+        q_emb = self.verb_q_emb(verb_q_idx)
 
-            verb_q_idx = verb_q_idx.view(batch_size*3, -1)
-            q_emb = self.verb_q_emb(verb_q_idx)
+        verb_pred_rep_prev = verb_pred_rep_prev.expand(3,verb_pred_rep_prev.size(0), verb_pred_rep_prev.size(1))
+        verb_pred_rep_prev = verb_pred_rep_prev.transpose(0,1)
+        verb_pred_rep_prev = verb_pred_rep_prev.contiguous().view(batch_size* 3, -1)
 
-            verb_pred_rep_prev = verb_pred_rep_prev.expand(3,verb_pred_rep_prev.size(0), verb_pred_rep_prev.size(1))
-            verb_pred_rep_prev = verb_pred_rep_prev.transpose(0,1)
-            verb_pred_rep_prev = verb_pred_rep_prev.contiguous().view(batch_size* 3, -1)
+        verb_pred_rep = self.verb_vqa(img_embd, q_emb)
+        combined = verb_pred_rep_prev + self.dropout(verb_pred_rep)
+        verb_pred = self.last_class(combined)
 
-            verb_pred_rep = self.verb_vqa(img_embd, q_emb)
-            combined = verb_pred_rep_prev + self.dropout(verb_pred_rep)
-            verb_pred = self.last_class(combined)
+        verb_pred = verb_pred.contiguous().view(batch_size, -1, self.n_verbs)
 
-            verb_pred = verb_pred.contiguous().view(batch_size, -1, self.n_verbs)
+        loss2 = (self.calculate_loss(verb_pred[:,0], verbs) + self.calculate_loss(verb_pred[:,1], verbs) +
+                 self.calculate_loss(verb_pred[:,2], verbs)) /3
 
-            loss2 = (self.calculate_loss(verb_pred[:,0], verbs) + self.calculate_loss(verb_pred[:,1], verbs) +
-                     self.calculate_loss(verb_pred[:,2], verbs)) /3
-
-            sum_losses += loss2
-        batch_avg_loss = sum_losses / 4
+        sum_losses = loss1 + loss2
+        batch_avg_loss = sum_losses / 2
         loss = batch_avg_loss
 
         return verb_pred, loss
@@ -206,28 +203,30 @@ class BaseModel(nn.Module):
 
         q_emb = self.verb_q_emb(verb_q_idx)
 
-        verb_pred_logit_prev = self.verb_vqa(img_embd, q_emb)
-        verb_pred_prev = self.last_class(verb_pred_logit_prev)
-
-        sorted_idx = torch.sort(verb_pred_prev, 1, True)[1]
-        verbs = sorted_idx[:,0]
-        role_pred, pred_rep = self.role_module(img, verbs)
-        label_idx = torch.max(role_pred,-1)[1]
-
-        verb_q_idx = self.encoder.get_verbq_idx(verbs, label_idx)
-
-        if self.gpu_mode >= 0:
-            verb_q_idx = verb_q_idx.to(torch.device('cuda'))
-
-        '''img_embd = self.conv(img)
-        batch_size, n_channel, conv_h, conv_w = img_embd.size()
-        img_embd = img_embd.view(batch_size, n_channel, -1)
-        img_embd = img_embd.permute(0, 2, 1)'''
-
-        q_emb = self.verb_q_emb(verb_q_idx)
-
-        verb_pred_logit = self.verb_vqa(img_embd, q_emb) + verb_pred_logit_prev
+        verb_pred_logit = self.verb_vqa(img_embd, q_emb)
         verb_pred = self.last_class(verb_pred_logit)
+
+        for i in range(3):
+
+            sorted_idx = torch.sort(verb_pred, 1, True)[1]
+            verbs = sorted_idx[:,0]
+            role_pred, pred_rep = self.role_module(img, verbs)
+            label_idx = torch.max(role_pred,-1)[1]
+
+            verb_q_idx = self.encoder.get_verbq_idx(verbs, label_idx)
+
+            if self.gpu_mode >= 0:
+                verb_q_idx = verb_q_idx.to(torch.device('cuda'))
+
+            '''img_embd = self.conv(img)
+            batch_size, n_channel, conv_h, conv_w = img_embd.size()
+            img_embd = img_embd.view(batch_size, n_channel, -1)
+            img_embd = img_embd.permute(0, 2, 1)'''
+
+            q_emb = self.verb_q_emb(verb_q_idx)
+
+            verb_pred_logit = self.verb_vqa(img_embd, q_emb) + verb_pred_logit
+            verb_pred = self.last_class(verb_pred_logit)
 
         return verb_pred
 
