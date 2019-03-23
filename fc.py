f3f5a22edb13098a9bfcdae5e36aecaf153bf1c5
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-03-23 22:38:15 +0800

    training from beginning role

--- fc.py
+++ fc.py
@@ -8,6 +8,34 @@ from torch.nn.utils.weight_norm import weight_norm
 from torch.nn.init import kaiming_uniform_, xavier_uniform_, normal
 
 
+class FCNet1(nn.Module):
+    """Simple class for non-linear fully connect network
+    """
+    def linear(self, in_dim, out_dim, bias=True):
+        lin = nn.Linear(in_dim, out_dim, bias=bias)
+        xavier_uniform_(lin.weight)
+        if bias:
+            lin.bias.data.zero_()
+
+        return lin
+
+    def __init__(self, dims):
+        super(FCNet1, self).__init__()
+
+        layers = []
+        for i in range(len(dims)-2):
+            in_dim = dims[i]
+            out_dim = dims[i+1]
+            layers.append(weight_norm(self.linear(in_dim, out_dim), dim=None))
+            layers.append(nn.ReLU())
+        layers.append(weight_norm(self.linear(dims[-2], dims[-1]), dim=None))
+        layers.append(nn.ReLU())
+
+        self.main = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.main(x)
+
 class FCNet(nn.Module):
     """Simple class for non-linear fully connect network
     """
@@ -26,9 +54,9 @@ class FCNet(nn.Module):
         for i in range(len(dims)-2):
             in_dim = dims[i]
             out_dim = dims[i+1]
-            layers.append(weight_norm(self.linear(in_dim, out_dim), dim=None))
+            layers.append(self.linear(in_dim, out_dim))
             layers.append(nn.ReLU())
-        layers.append(weight_norm(self.linear(dims[-2], dims[-1]), dim=None))
+        layers.append(self.linear(dims[-2], dims[-1]))
         layers.append(nn.ReLU())
 
         self.main = nn.Sequential(*layers)
