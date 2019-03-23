84d7f54327889e6cbaaedf36502df4bbf5bd7642
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-03-23 22:33:57 +0800

    training from beginning role

--- model_roles_working.py
+++ model_roles_working.py
@@ -39,8 +39,12 @@ class TopDown(nn.Module):
         '''self.q_emb = nn.LSTM(embed_hidden, mlp_hidden,
                              batch_first=True, bidirectional=True)
         self.lstm_proj = nn.Linear(mlp_hidden * 2, mlp_hidden)'''
-        self.v_att = Attention(mlp_hidden, embed_hidden, mlp_hidden)
-        self.q_net = FCNet([embed_hidden, mlp_hidden])
+        self.q_proj = nn.Sequential(
+            nn.Linear(embed_hidden*2, mlp_hidden),
+            nn.ReLU(inplace=True),
+        )
+        self.v_att = Attention(mlp_hidden, mlp_hidden, mlp_hidden)
+        self.q_net = FCNet([mlp_hidden, mlp_hidden])
         self.v_net = FCNet([mlp_hidden, mlp_hidden])
         self.classifier = SimpleClassifier(
             mlp_hidden, 2 * mlp_hidden, self.vocab_size, 0.5)
@@ -53,7 +57,7 @@ class TopDown(nn.Module):
         lstm_out, (h, _) = self.q_emb(w_emb)
         q_emb = h.permute(1, 0, 2).contiguous().view(batch_size, -1)
         q_emb = self.lstm_proj(q_emb)'''
-        q_emb = q
+        q_emb = self.q_proj(q)
 
         att = self.v_att(img, q_emb)
         v_emb = (att * img).sum(1) # [batch, v_dim]
@@ -136,7 +140,7 @@ class BaseModel(nn.Module):
         verb_embed_expand = verb_embd.expand(self.max_role_count, verb_embd.size(0), verb_embd.size(1))
         verb_embed_expand = verb_embed_expand.transpose(0,1)
         verb_embed_expand = verb_embed_expand.contiguous().view(-1, self.embed_hidden)
-        role_verb = role_embd.squeeze() * verb_embed_expand
+        role_verb = torch.cat([role_embd.squeeze(), verb_embed_expand], -1)
 
         logits = self.roles(img, role_verb)
 
