775a4458cb97bfd71c6b67c497936fe4ca85b871
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-04-25 22:35:32 +0800

    train cnn

--- main_ft_cnn4imsitu.py
+++ main_ft_cnn4imsitu.py
@@ -11,7 +11,7 @@ import random
 #from graphviz import Digraph
 
 
-def train(model, train_loader, dev_loader, traindev_loader, optimizer, scheduler, max_epoch, model_dir, encoder, gpu_mode, clip_norm, lr_max, model_name, args,eval_frequency=4):
+def train(model, train_loader, dev_loader, traindev_loader, optimizer, scheduler, max_epoch, model_dir, encoder, gpu_mode, clip_norm, lr_max, model_name, args,eval_frequency=4000):
     model.train()
     train_loss = 0
     total_steps = 0
@@ -168,7 +168,7 @@ def eval(model, dev_loader, encoder, gpu_mode, write_to_file = False):
 
 
             del img, labels
-            break
+            #break
 
     #return top1, top5, val_loss/mx
 
