2e5f952c2c409de44bd66ab8c3cd4a1f46ab7991
thilinicooray <thilinicooray.ucsc@gmail.com>
2019-05-12 10:54:25 +0800

    train cnn

--- main_vgg_featextractor4ggnn.py
+++ main_vgg_featextractor4ggnn.py
@@ -86,8 +86,8 @@ def main():
         utils.load_net(args.resume_model, [model])'''
 
     #load verb and role modules
-    utils.load_net(args.verb_module, [model.conv_verbs])
-    utils.load_net(args.role_module, [model.conv_nouns])
+    utils.load_net(args.verb_module, [model.conv_verbs], ['conv_verbs'])
+    utils.load_net(args.role_module, [model.conv_nouns], ['conv_nouns'])
 
 
     if args.gpuid >= 0:
